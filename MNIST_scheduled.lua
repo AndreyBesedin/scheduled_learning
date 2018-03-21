ed43cb2e942283934e544585fb483de086f3f03a
AndreyBesedin <andreibes89@gmail.com>
2018-03-21 12:10:38 +0100

    added MNIST

--- MNIST_scheduled.lua
+++ MNIST_scheduled.lua
@@ -8,33 +8,38 @@ opt = {
   valBatchSize = 60,
   batches_per_env = 1000,
   epochs = 1000,
-  lrC = 0.005,
-  lrW = 0.1,
-  nb_classes = 4,
+  lrC = 0.0005,
+  lrW = 10,
+  nb_classes = 10,
   load_pretrained_classifier = true,
   load_pretrained_weighter = true,
   train = 'full',
   cuda = true,
-  dataset = 'quaters', -- MNIST 
+  dataset = 'MNIST', -- MNIST 
   optimizeW = true,
   nb_env = 200
 }
 opt.manualSeed = torch.random(1, 10000) -- fix seed
 torch.manualSeed(opt.manualSeed)
 
-data_path = './synthetic_data/points_2D/quaters_2D/'
-trainset = torch.load(data_path .. 'train123.t7')
-valset = torch.load(data_path .. 'val.t7')
-testset = torch.load(data_path .. 'test.t7')
+data_path = './MNIST/t7_files/'
+trainset = torch.load(data_path .. 'trainset.t7')
+valset = torch.load(data_path .. 'validation.t7')
+testset = torch.load(data_path .. 'testset.t7')
 --------------------------------------------------------------------------------------------------------------------------------
 -- DEFINING THE CLASSIFIER
 --------------------------------------------------------------------------------------------------------------------------------
 if opt.load_pretrained_classifier then
-  netC = torch.load('./pretrained_models/C_full_quaters2D.t7')
+  netC = torch.load('./pretrained_models/MNIST_stream_classifier.t7')
   pC, gpC = netC:getParameters()
 else
   netC = nn.Sequential()
-  netC:add(nn.Linear(2,10)):add(nn.ReLU(true)):add(nn.Linear(10,10)):add(nn.ReLU(true)):add(nn.Linear(10,4)):add(nn.LogSoftMax())
+  netC:add(nn.SpatialConvolution(1,16, 4, 4, 2, 2, 1, 1)):add(nn.ReLU(true))
+  netC:add(nn.SpatialConvolution(16, 32, 4, 4, 2, 2, 1, 1)):add(nn.ReLU(true))
+  netC:add(nn.SpatialMaxPooling(32, 2, 2, 2,2)):add(nn.View(512))
+  netC:add(nn.Linear(512,512)):add(nn.dropout(0.5)):add(nn.ReLU(true))
+  netC:add(nn.Linear(512,128)):add(nn.ReLU(true))
+  netC:add(nn.Linear(128,10)):add(nn.ReLU(true)):add(nn.LogSoftMax())
   pC, gpC = netC:getParameters()
   pC = pC:normal()
 end
@@ -47,7 +52,8 @@ if opt.load_pretrained_weighter then
   pW, gpW = netC:getParameters()
 else
   netW = nn.Sequential()
-  netW:add(nn.Linear(opt.nb_classes,20)):add(nn.ReLU(true)):add(nn.Linear(20,20)):add(nn.ReLU(true)):add(nn.Linear(20,p:size(1))):add(nn.Sigmoid())
+  netW:add(nn.Linear(opt.nb_classes,256)):add(nn.ReLU(true))
+  netW:add(nn.Linear(256,pC:size(1))):add(nn.Sigmoid())
   pW, gpW = netW:getParameters()
 end
 critC = nn.ClassNLLCriterion(); 
@@ -69,8 +75,7 @@ stream = true
 env_count = 0
 res_conf = torch.FloatTensor(opt.nb_env)
 while env_count < opt.nb_env do
-  --current_env = getEnv(opt)
-  current_env = torch.CudaTensor({0,1,1,1})
+  current_env = getEnv(opt)
   env_count = env_count + 1
   print('Current environment: '); print(current_env:reshape(1,4))
   if opt.optimizeW == true then
@@ -89,28 +94,22 @@ while env_count < opt.nb_env do
     df_dtheta = critC:backward(output, batch.labels)
     netC:backward(batch.data, df_dtheta)   -- (3)
     if opt.optimizeW == true then
-      grad_norm = 1
-      while grad_norm > 0.005 do
-        netC_temp = netC:clone(); pC_temp, gpC_temp = netC_temp:getParameters()
-        --gpC:clamp(-10,10)
-        pC_temp:add(-opt.lrC, torch.cmul(W, gpC));      -- (4)
-        val_indices = torch.randperm(valset.data:size(1))[{{1, opt.valBatchSize}}]
-        batch_val = getBatch(valset, val_indices, opt)       -- (5)
-        output_val = netC_temp:forward(batch_val.data)  -- (6)
-        df_dtheta = critC:backward(output_val, batch_val.labels)
-        netC_temp:backward(batch_val.data, df_dtheta); -- gpC:clamp(-5,5)
-        dW = -opt.lrC*torch.cmul(gpC_temp, gpC)                 -- (7.2)
-        gpW:zero()
-        dW_dgamma = critW:backward(W, W-dW)
-        netW:backward(current_env, dW_dgamma)
-        grad_norm = gpW:norm()
-        --print('Gradient norm for W: ' .. grad_norm)
-        pW:add(-opt.lrW, gpW)
-        W = netW:forward(current_env)
-      end
+      netC_temp = netC:clone(); pC_temp, gpC_temp = netC_temp:getParameters()
+      --gpC:clamp(-10,10)
+      pC_temp:add(-opt.lrC, torch.cmul(W, gpC));      -- (4)
+      val_indices = torch.randperm(valset.data:size(1))[{{1, opt.valBatchSize}}]
+      batch_val = getBatch(valset, val_indices, opt)       -- (5)
+      output_val = netC_temp:forward(batch_val.data)  -- (6)
+      df_dtheta = critC:backward(output_val, batch_val.labels)
+      netC_temp:backward(batch_val.data, df_dtheta); -- gpC:clamp(-5,5)
+      dW = -opt.lrC*torch.cmul(gpC_temp, gpC)                 -- (7.2)
+      gpW:zero()
+      dW_dgamma = critW:backward(W, W-dW)
+      netW:backward(current_env, dW_dgamma)
+      pW:add(-opt.lrW, gpW)
+      W = netW:forward(current_env)
     end
     pC:add(-opt.lrC, torch.cmul(W, gpC))
-    confusion = test_classifier(netC, testset, opt); print(confusion)
   end
   print("Sum of parameters of W: " .. W:sum())
   confusion = test_classifier(netC, testset, opt); print(confusion)
